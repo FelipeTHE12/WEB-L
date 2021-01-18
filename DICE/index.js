b5fb7d0b57f664485acb459e08062810181f1e14
Felipe <felipeoliveirasilva12@outlook.com>
2021-01-18 10:29:09 -0300

    dice game page

---
+++ DICE/index.js
@@ -0,0 +1,12 @@
+var randomNumber1= Math.floor(Math.random()*6)+1;
+var randomDiceImage = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceImage)
+
+var randomNumber2 = Math.floor(Math.random()*6)+1;
+var randomDiceImage2 = "images/dice" +randomNumber2 + ".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+
+if (randomNumber1 < randomNumber2){document.querySelector("h1").innerHTML = " Jogador 2 venceu !!"}
+else if(randomNumber1 > randomNumber2){document.querySelector("h1").innerHTML = "Jogador 1 venceu!!";}
+else {document.querySelector("h1").innerHTML = "Empate!!";}
